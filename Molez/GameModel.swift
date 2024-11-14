ebcd5f9446d866548f7d4c31aad103d07ee96989
jxiang0608 <cxiang2@u.rochester.edu>
2024-11-14 13:11:54 -0500

    created the game model

---
+++ Molez/GameModel.swift
@@ -0,0 +1,38 @@
+//
+//  GameModel.swift
+//  Molez
+//
+//  Created by Joy Xiang on 11/14/24.
+//
+
+import Foundation
+
+class GameModel {
+    var score: Int
+    var remainingTime: Int
+    var gameOver: Bool
+    
+    private let initialTime = 15
+    
+    init() {
+        self.score = 0
+        self.remainingTime = initialTime
+        self.gameOver = true
+    }
+    
+    func startGame() {
+        score = 0
+        remainingTime = initialTime
+        gameOver = false
+    }
+    
+    func updateTime() {
+        if remainingTime > 0 {
+            remainingTime -= 1
+        }
+        
+        if remainingTime == 0 {
+            gameOver = true
+        }
+    }
+}
