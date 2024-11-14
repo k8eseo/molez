d43c01ee85f4baddcd2f0cf724f646c3c8f9d6b7
jxiang0608 <cxiang2@u.rochester.edu>
2024-11-14 10:12:11 -0500

    Initial Commit

---
+++ MolezUITests/MolezUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MolezUITestsLaunchTests.swift
+//  MolezUITests
+//
+//  Created by Joy Xiang on 11/14/24.
+//
+
+import XCTest
+
+final class MolezUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
